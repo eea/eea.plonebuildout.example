fefad7afdc03e74349beb78498ed3c11c61efa79
Olimpiu Rob <olimpiu.rob@gmail.com>
2015-03-05 18:00:08 +0200

    Refs #22402; Updated reset_buildout.sh

--- reset_buildout.sh
+++ reset_buildout.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+
+LASTCFG=''
+
+if [ -s ".mr.developer.cfg"]; then
+    LASTCFG=`sed -n "/\[buildout\]/ {N; /\.*args\ =/ N; n; p;}" .mr.developer.cfg| sed -e 's/^[ \t]*//'| sed "s/'//g"`
+fi
+
 rm -rf ./bin ./develop-eggs ./include ./lib ./parts ./.installed.cfg
-./install.sh
+
+echo "$LASTCFG"
+
+if [ ! -z "$LASTCFG" ]; then
+    echo "Not empty"
+    if [ -s "$LASTCFG" ]; then
+        echo ""
+        echo "Last buildout configuration used was: $LASTCFG. Using the same config file for reinstallation"
+        echo ""
+    fi
+fi
+
+./install.sh $LASTCFG
