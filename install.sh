424275c80bbe474dfb7f1d77efeb76efeb4966b9
Olimpiu Rob <olimpiu.rob@gmail.com>
2015-03-05 14:34:01 +0200

    Added buildout config parameter to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+CONFIG=$1
+
+if [ -z "$CONFIG" ]; then
+  if [ -s "development.cfg" ]; then
+    CONFIG="development.cfg"
+  else
+    CONFIG="buildout.cfg"
+  fi
+fi
+
 wget -O bootstrap.sh https://raw.githubusercontent.com/olimpiurob/eea.plonebuildout.core/master/install.sh
 chmod u+x bootstrap.sh
-./bootstrap.sh
+./bootstrap.sh $CONFIG
